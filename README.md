980e6ea439897f5ca0a364345932c63c2bdca73f
telorand <telorand@users.noreply.github.com>
2019-11-08 21:50:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Intro-to-Python
+ Python Learning!
---
+++ Strings.py
@@ -0,0 +1,10 @@
+# first_name = 'Matt'
+# last_name = 'Bullock'
+
+first_name = input('Please enter your first name: ')
+last_name = input('Please enter your last name: ')
+output = f'Hello, {first_name} {last_name}'
+
+# print(first_name + last_name)
+# print('Hello', first_name, last_name + '!')
+print(output)
